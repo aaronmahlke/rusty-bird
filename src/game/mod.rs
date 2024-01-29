99edf1e296b24abf7a7a42620f864e5f12803644
Aaron Mahlke <aaron.mahlke@gmail.com>
2024-01-29 12:08:02 +0100

    Adds fps counter

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,4 +1,5 @@
 pub mod base;
 pub mod bird;
+pub mod fps;
 pub mod pipes;
 pub mod walls;
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-use bevy::prelude::*;
-
+use bevy::{
+    diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
+    prelude::*,
+    window::{PresentMode, WindowLevel, WindowTheme},
+};
 mod game;
 
 use crate::game::base::*;
@@ -7,20 +10,35 @@ use crate::game::bird::*;
 use crate::game::pipes::*;
 use crate::game::walls::*;
 
+use crate::game::fps::*;
+
 struct GamePlugin;
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins((BasePlugin, WallsPlugin, BirdPlugin, PipesPlugin));
+        app.add_plugins((BasePlugin, WallsPlugin, BirdPlugin, PipesPlugin, FPSPlugin));
     }
 }
 const BACKGROUND_COLOR: Color = Color::rgb(0.9, 0.9, 0.9);
 
 fn main() {
     App::new()
-        .add_plugins((DefaultPlugins, GamePlugin))
+        .add_plugins((
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "Rusty Bird".into(),
+                    present_mode: PresentMode::Immediate,
+                    prevent_default_event_handling: false,
+                    window_theme: Some(WindowTheme::Dark),
+                    ..default()
+                }),
+                ..default()
+            }),
+            GamePlugin,
+        ))
+        .add_plugins(FrameTimeDiagnosticsPlugin)
         .insert_resource(ClearColor(BACKGROUND_COLOR))
         .add_systems(Startup, setup)
-        .run()
+        .run();
 }
 
 fn setup(mut commands: Commands) {
